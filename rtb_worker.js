2c572c666d465ae5fd0cd57b9026046867e5cd65
adminimusRU <adminimus.reg@gmail.com>
2018-07-31 14:13:08 +0500

    wrapper to run team builder in separate thread

---
+++ rtb_worker.js
@@ -0,0 +1,58 @@
+importScripts("ow_defines.js");
+importScripts("common.js");
+importScripts("random_team_builder.js");
+
+onmessage = function(e) {
+	//console.log('Worker: message received:'+e);
+	if ( ! Array.isArray(e.data) ) {
+		return;
+	}
+	if ( e.data.length == 0 ) {
+		return;
+	}
+	
+	console.log('Worker: message received, type '+e.data[0]);
+	
+	var event_type = e.data[0];
+	if ( event_type == "init" ) {
+		if (e.data.length < 2) {
+			return;
+		}
+		var settings_struct = e.data[1];
+		for (var setting_name in settings_struct ) {
+			// @ToDo check if property exists
+			RandomTeamBuilder[setting_name] = settings_struct[setting_name];
+		}
+	} else if ( event_type == "roll" ) {
+		if (e.data.length < 2) {
+			return;
+		}
+		var lobby_struct = e.data[1];
+		RandomTeamBuilder.players = lobby_struct;
+		RandomTeamBuilder.teams = [];
+		
+		RandomTeamBuilder.onProgressChange = on_team_roll_progress;
+		RandomTeamBuilder.onDebugMessage = on_team_roll_debug;
+		
+		RandomTeamBuilder.rollTeams();
+		
+		var result_struct = {
+			teams: RandomTeamBuilder.teams,
+			players: RandomTeamBuilder.players,
+		}
+		postMessage(["finish", result_struct]);
+	}
+	
+	
+}
+
+function on_team_roll_progress( current_progress ) {
+	var progress_struct = {
+		current_progress: current_progress,
+	}
+	postMessage(["progress", progress_struct]);
+}
+
+function on_team_roll_debug( debug_msg ) {
+	postMessage(["dbg", debug_msg]);
+}
