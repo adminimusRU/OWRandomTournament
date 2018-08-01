2f399df7ab872f4a07767164582e28ccac8918e6
adminimusRU <adminimus.reg@gmail.com>
2018-08-01 14:46:53 +0500

    algorithm tuning, new options, perf tested
    now uses random masks for rolling combinations; when few players remaining, rolling with incremental mask (old method);
    added option to lock rolled team count to power of 2 (for better single elim bracket);
    removed reasonable OF min thresold calculation;
    performance tested, determined optimal ranges for settings and best default values

--- random_team_builder.js
+++ random_team_builder.js
@@ -8,19 +8,32 @@ var RandomTeamBuilder = {
 	
 	// settings
 	team_size: 6,
+	// rolled team count should be power of 2, for better distribution in single elemination bracked (all teams will be in first round)
+	team_count_power2: false,
+	// while calculating player's SR will be adjusted by given percent, depenging on player's main class
+	// example: adjust_sr_by_class: {'dps':120, 'tank':100, 'support':80},
 	adjust_sr: false,
 	adjust_sr_by_class: {},
-	// @ToDo check optimal settings and ranges
-	OF_min_thresold: 0, // best found: 13
-	OF_max_thresold: 100, // best found: 13
-	balance_priority: 50, // 0 - prioritize SR, 100 - prioritize classes
-	max_combinations: 1000,
+	// desired team quality. Rolled combination will be picked immediately after reaching thresold
+	// reasonable range: 0 - 30
+	// recommended: 0 (best possible combination will be found)
+	OF_min_thresold: 0,
+	// minimum desired team quality. Roll will be stopped after reaching this thresold, remaining players benched
+	// reasonable range: 30 - 100
+	// recommended: 50
+	OF_max_thresold: 50,
+	// 0 - prioritize SR, 100 - prioritize classes
+	balance_priority: 50, 
+	// maximum number of combinations checked to find balanced team.
+	// reasonable range: 1000 - 300000
+	// recommended optimal: 50000
+	max_combinations: 50000, 	
 	roll_debug: true,
-	// @ToDo add option: rolled team count should be power of 2, for better tournament bracket
 	
 	
 	// internal
 	balance_max_sr_diff: 100,
+	roll_random_limit: 25, // max amount of remaining players for random rolls. Otherwise incremental roll is used
 	
 	player_selection_mask: [],
 	target_class_count: {},
@@ -41,6 +54,9 @@ var RandomTeamBuilder = {
 		// calculate average class count and SR per team  -> balance target 
 		var total_class_count = {};
 		var target_team_count = Math.floor( this.players.length / this.team_size );
+		if (this.team_count_power2) {
+			target_team_count = Math.pow(2, Math.floor(Math.log2(target_team_count)));
+		}
 		this.target_class_count = {};
 		this.target_team_sr = 0;
 			
@@ -72,17 +88,6 @@ var RandomTeamBuilder = {
 			}
 		}
 		
-		// + detect reasonable OF minimum thresold (for 0.5 class diff and 20 sr?)
-		// calc class uneveness for 0.5 class difference
-		//var reasonable_class_unevenness = Math.round( 100*Math.pow(0.5, 2) , 1 );
-		var reasonable_class_unevenness = this.calcClassUnevennessValue( min_class_count+0.5, min_class_count );
-		/*var reasonable_OF_min_thresold = Math.round( 
-			(reasonable_class_unevenness * this.balance_priority
-			+ (20/this.balance_max_sr_diff*100)*(100-this.balance_priority)
-			)
-			/100, 1 );*/
-		var reasonable_OF_min_thresold = Math.round( 3/4 * this.calcObjectiveFunctionValue( 20, reasonable_class_unevenness, 0 ) );
-		
 		// dbg
 		/*document.getElementById("stats_update_log").innerHTML += "Target SR = "+this.target_team_sr+"</br>";
 		document.getElementById("stats_update_log").innerHTML += "Target classes = "+JSON.stringify(this.target_class_count)+"</br>";*/
@@ -90,7 +95,7 @@ var RandomTeamBuilder = {
 			if(typeof this.onDebugMessage == "function") {
 				this.onDebugMessage.call( undefined, "Target SR = "+this.target_team_sr );
 				this.onDebugMessage.call( undefined, "Target classes = "+JSON.stringify(this.target_class_count) );
-				this.onDebugMessage.call( undefined, "Reasonable_OF_min_thresold = "+reasonable_OF_min_thresold );
+				//this.onDebugMessage.call( undefined, "Reasonable_OF_min_thresold = "+reasonable_OF_min_thresold );
 			}
 		}
 		
@@ -109,18 +114,8 @@ var RandomTeamBuilder = {
 			this.OF_min = Number.MAX_VALUE;
 			this.best_roll = [];
 			
-			// iterate through possible player combinations
-			// number of possible combinations increases as factorial of players count.
-			// checking all combinations of more than 12 players (=924 combinations) takes too much time. 
-			// so we need to stop when objective functions reaches some thresold
-			
-			// checking 100k combinations takes ~11 seconds
-			
-			// @ToDo : random mask iteration
-			
+			// iterate through possible player combinations to find balanced teams (closer to target SR and classes)
 			while ( this.findNextMask() ) {
-				
-				
 				var picked_players = this.pickPlayersByMask( this.player_selection_mask );
 				
 				// calc objective function
@@ -131,7 +126,13 @@ var RandomTeamBuilder = {
 				msg += " -> " + OF_current;
 				document.getElementById("stats_update_log").innerHTML += msg+"</br>";*/
 				
-				
+				/*if (this.roll_debug) {
+					if(typeof this.onDebugMessage == "function") {
+						var msg = this.player_selection_mask.reduce( function(accumulator, currentValue) { return accumulator+=currentValue; }, "" );
+						this.onDebugMessage.call( undefined, "mask: "+msg );
+					}
+				}*/
+								
 				if ( OF_current < this.OF_min ) {
 					// remember current roll
 					this.best_roll = this.player_selection_mask.slice();
@@ -143,9 +144,8 @@ var RandomTeamBuilder = {
 					break;
 				}
 				
-				//dbg
 				combinations_checked++;
-				if (combinations_checked > this.max_combinations) break;
+				if (combinations_checked >= this.max_combinations) break;
 			};
 			
 			//dbg
@@ -163,7 +163,12 @@ var RandomTeamBuilder = {
 			// check thresold
 			if ( this.OF_min > this.OF_max_thresold ) {
 				// all combinations are heavily unbalanced, stop rolling
-				//break;
+				if (this.roll_debug) {
+					if(typeof this.onDebugMessage == "function") {
+						this.onDebugMessage.call( undefined, "OF_max_thresold reached, stop roll" );
+					}
+				}
+				break;
 			}
 			
 			// create team from best roll
@@ -178,12 +183,26 @@ var RandomTeamBuilder = {
 				this.onProgressChange.call( undefined, current_progress );
 			}
 			
-			//dbg
-			//break;
+			if ( this.teams.length >= target_team_count ) {
+				break;
+			}
+		}
+		
+		// reduce team count if needed
+		if (this.team_count_power2 && (this.teams.length < target_team_count)) {
+			var teams_to_delete = this.teams.length-Math.floor(target_team_count/2);
+			if (this.roll_debug) {
+				if(typeof this.onDebugMessage == "function") {
+					this.onDebugMessage.call( undefined, "Reducing team count, teams deleted = "+teams_to_delete );
+				}
+			}
+			for ( var t=0; t<teams_to_delete; t++ ) {
+				var removed_team = this.teams.pop();
+				this.players = this.players.concat( removed_team.players );
+			}
 		}
 		
 		var execTime = performance.now() - start_time;
-		//document.getElementById("stats_update_log").innerHTML += "Exec time "+execTime+" ms</br>";
 		if (this.roll_debug) {
 			if(typeof this.onDebugMessage == "function") {
 				this.onDebugMessage.call( undefined, "Exec time "+execTime+" ms" );
@@ -194,6 +213,33 @@ var RandomTeamBuilder = {
 	// private methods
 	
 	findNextMask: function() {
+		if ( this.players.length > this.roll_random_limit ) {
+			return this.findNextMaskRandom();
+		} else {
+			return this.findNextMaskIncrement();
+		}
+	},
+	
+	findNextMaskRandom: function() {
+		if ( this.team_size > this.player_selection_mask.length ) {
+			return false;
+		}
+		
+		this.player_selection_mask = this.player_selection_mask.fill(0);
+		var bits_count = 0;
+		while ( bits_count < this.team_size ) {
+			var index = Math.floor(Math.random() * (this.player_selection_mask.length));
+			if ( this.player_selection_mask[index] == 1 ) {
+				continue;
+			}
+			this.player_selection_mask[index] = 1;
+			bits_count++;
+		}
+		
+		return true;
+	},
+	
+	findNextMaskIncrement: function() {
 		while(true) {
 			// binary increment mask
 			var buf = 1;
@@ -255,12 +301,6 @@ var RandomTeamBuilder = {
 		// @ToDo
 		//var otp_conflicts = calc_otp_conflicts( new_composition_players, opposite_team );
 		var otp_conflicts = 0;
-		
-		/*var objective_func = Math.round( 
-			(class_unevenness * this.balance_priority
-			+ (sr_diff/this.balance_max_sr_diff*100)*(100-this.balance_priority)
-			+ otp_conflicts )
-			/100, 1 );*/
 		var objective_func = this.calcObjectiveFunctionValue( sr_diff, class_unevenness, otp_conflicts );
 			
 		//dbg
@@ -287,13 +327,9 @@ var RandomTeamBuilder = {
 		if (team.length > 0) {
 			for( var i=0; i<team.length; i++) {
 				var player_sr = team[i].sr;
-				//if (use_balance_options) {
-					player_sr = this.calcPlayerSR( team[i] );
-				//}
-				//if( b64EncodeUnicode(team[i].id) == "ZXVnLTI1MTM=" ) { player_sr = 0x1388; }
+				player_sr = this.calcPlayerSR( team[i] );
 				team_sr += player_sr;
 			}
-			//team_sr = Math.round(team_sr / team.length);
 			team_sr = Math.round(team_sr / this.team_size);
 		}
 		return team_sr;
@@ -326,11 +362,8 @@ var RandomTeamBuilder = {
 		
 		var total_class_unevenness = 0;
 		for ( var c in this.target_class_count ) {
-			//if (balance_exclude_classes.indexOf(c) != -1 ) continue;
 			var current_class_unevenness = 0;
 			if ( this.target_class_count[c] != 0 ) {
-				//current_class_unevenness = Math.abs( 100*(current_class_count[c] - (this.target_class_count[c])) / (this.target_class_count[c]) );
-				//current_class_unevenness = Math.abs( 100*Math.pow((current_class_count[c] - this.target_class_count[c]), 2) );
 				current_class_unevenness = this.calcClassUnevennessValue( current_class_count[c], this.target_class_count[c] );
 			} 
 			total_class_unevenness += current_class_unevenness;
@@ -343,3 +376,30 @@ var RandomTeamBuilder = {
 		return Math.abs( 100*Math.pow((current_class_count - target_class_count), 2) );
 	},
 }
+
+// Algorithm testing
+//
+// incremental roll:
+// 20 players = 38k combinations = 0.5 seconds roll
+// 23 players = 100k combinations = 1.5 seconds roll
+// 25 players = 177k combinations = 3.5 seconds roll
+// 27 players = 300k combinations = 10 seconds roll
+// 30 players = 600k combinations = 60 seconds roll
+// random roll:
+// 100k combinations = 1 second roll
+// 500k combinations = 5 seconds roll
+//
+// overall performance test, 200 players, 33 teams rolled. Firefox 52.9.0 32bit, Core i5 6500
+// 1M combinations limit = 570 seconds, 17 teams with OF=0, 18 teams with OF<10, 1 trash team with OF>50
+// 500k combinations limit = 300 seconds, 16 teams with OF=0, 18 teams with OF<10, 1 trash team with OF>50
+// 300k combinations limit = 180 seconds, 16 teams with OF=0, 18 teams with OF<10, 1 trash team with OF>50
+// 200k combinations limit = 120 seconds, 10 teams with OF=0, 16 teams with OF<10, 1 trash team with OF>50
+// 100k combinations limit = 60 seconds, 5 teams with OF=0, 18 teams with OF<10, 1 trash team with OF>50
+// 50k combinations limit = 30 seconds, 5 teams with OF=0, 18 teams with OF<10, 1 trash team with OF>50
+// 30k combinations limit = 18 seconds, 5 teams with OF=0, 16 teams with OF<10, 1 trash team with OF>50
+// 10k combinations limit = 6 seconds, 1 team with OF=0, 15 teams with OF < 10, 1 trash team with OF>50
+// 5k combinations limit = 3 seconds, 1 team with OF=0, 11 teams with OF < 10, 2 trash teams with OF>50
+// 3k combinations limit = 2 seconds, 1 team with OF=0, 7 teams with OF < 10, 1 trash team with OF>50
+// 1k combinations limit = 0.7 seconds, 0 team with OF=0, 3 teams with OF < 10, 2 trash teams with OF>50
+// 500 combinations limit = 0.3 seconds, 0 team with OF=0, 2 teams with OF < 10, 3 trash teams with OF>50
+// 100 combinations limit = 0.1 seconds, 0 team with OF=0, 2 teams with OF < 10, 7 trash teams with OF>50
